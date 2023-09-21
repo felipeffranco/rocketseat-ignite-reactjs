2357f3d090d93711c3730aa7b3cf6535f30adece
felipeffranco <felipeffranco@outlook.com>
2023-09-21 20:33:33 -0300

    no errors in react

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -15,21 +15,23 @@ export function Post({ author, publishedAt, content }) {
 
     const [newCommentText, setNewCommentText] = useState('')
 
+    //Published Date
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL HH:mm'h'", {
         locale: ptBR, 
     })
-
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
         locale: ptBR,
         addSuffix: true,
     })
 
+    //Function creating new comment
     function handleCreateNewComment() {
         event.preventDefault();
         setComments([...comments, newCommentText]);
         setNewCommentText('');
     }
 
+    //Function catch textarea changes on form
     function handleNewCommentChange() {
         setNewCommentText(event.target.value)
     }
@@ -52,9 +54,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph') { 
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type === 'link') {
-                        return <p><a href="#">{line.content}</a></p>;
+                        return <p key={line.content}><a href="#">{line.content}</a></p>;
                     }
                 })}
             </div>
@@ -75,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
         </article>
